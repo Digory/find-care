e145044e00d8842b7d57e366baadd1f4b097cf2b
Digory <digory1@googlemail.com>
2018-07-20 16:18:14 +0100

    Add CRUD methods to ServiceUser and Worker classes

---
+++ models/ServiceUser.rb
@@ -0,0 +1,55 @@
+require_relative('../db/sql_runner.rb')
+require_relative('Worker.rb')
+
+class ServiceUser
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+    @budget = options['budget'].to_f
+  end
+
+  def save()
+    sql = "INSERT INTO service_users(name, weekly_budget) VALUES($1, $2) RETURNING id"
+    values = [@name, @budget]
+    result = SqlRunner.run(sql,values)
+    @id = result.first['id'].to_i
+  end
+
+  def update()
+    sql = "UPDATE service_users SET (name, weekly_budget) = ($1, $2) WHERE id = $3"
+    values = [@name, @weekly_budget, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM service_users WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql,values)
+  end
+
+  def workers()
+    sql = "SELECT workers.* FROM workers INNER JOIN visits ON workers.id = visits.worker_id WHERE visits.service_user_id = $1"
+    values = [@id]
+    results = SqlRunner.run(sql, values)
+    return results.map{|worker_info| Worker.new(worker_info)}
+  end
+
+  def self.all()
+    sql = "SELECT * FROM service_users"
+    results = SqlRunner.run(sql)
+    return results.map{|service_user_info| ServiceUser.new(service_user_info)}
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM service_users WHERE id = $1"
+    values = [id]
+    result = SqlRunner.run(sql, values)
+    return ServiceUser.new(result.first)
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM service_users"
+    SqlRunner.run(sql)
+  end
+end
---
+++ models/Worker.rb
@@ -0,0 +1,58 @@
+require_relative('../db/sql_runner.rb')
+require_relative('ServiceUser.rb')
+
+class ServiceUser
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+    @gender = options['gender']
+    @can_drive = options['can_drive']
+    @hourly_rate = options['hourly_rate'].to_f
+    @experience = options['experience']
+  end
+
+  def save()
+    sql = "INSERT INTO workers(name, gender, can_drive, hourly_rate, experience) VALUES($1, $2, $3, $4, $5) RETURNING id"
+    values = [@name, @gender, @can_drive, @hourly_rate, @experience]
+    result = SqlRunner.run(sql,values)
+    @id = result.first['id'].to_i
+  end
+
+  def update()
+    sql = "UPDATE workers SET (name, gender, can_drive, hourly_rate, experience) = ($1, $2, $3, $4, $5) WHERE id = $6"
+    values = [@name, @gender, @can_drive, @hourly_rate, @experience @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM workers WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql,values)
+  end
+
+  def service_users()
+    sql = "SELECT service_users.* FROM service_users INNER JOIN visits ON service_user.id = visits.service_user_id WHERE visits.worker_id = $1"
+    values = [@id]
+    results = SqlRunner.run(sql, values)
+    return results.map{|service_user_info| ServiceUser.new(service_user_info)}
+  end
+
+  def self.all()
+    sql = "SELECT * FROM workers"
+    results = SqlRunner.run(sql)
+    return results.map{|worker_info| Worker.new(worker_info)}
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM workers WHERE id = $1"
+    values = [id]
+    result = SqlRunner.run(sql, values)
+    return ServiceUser.new(result.first)
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM workers"
+    SqlRunner.run(sql)
+  end
+end
