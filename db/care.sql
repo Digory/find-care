7f89967c9eded3b1dfa8f3716d6a5260a3b8e899
Digory <digory1@googlemail.com>
2018-07-20 15:21:14 +0100

    Create database

---
+++ db/care.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS visits;
+DROP TABLE IF EXISTS service_users;
+DROP TABLE IF EXISTS workers;
+
+CREATE TABLE service_users(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  weekly_budget DECIMAL(8,2)
+);
+
+CREATE TABLE workers(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  gender CHAR(1),
+  can_drive BOOLEAN,
+  hourly_rate DECIMAL(5,2),
+  experience TEXT
+);
+
+CREATE TABLE visits(
+  id SERIAL8,
+  service_user_id INT8 REFERENCES service_users(id),
+  worker_id INT8 REFERENCES workers(id)
+);
