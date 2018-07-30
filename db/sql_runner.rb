1ecb7b3228383fcc8cd433f60a60308c9677bd26
Digory <digory1@googlemail.com>
2018-07-30 16:53:01 +0100

    Changes for Heroku

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -15,7 +15,7 @@ class SqlRunner
 
   def self.run(sql, values = [])
     begin
-      db = PG.connect({dbname: 'ddaa7cf7uq3tqn', host: 'ec2-23-21-216-174.compute-1.amazonaws.com', port: '5432', user: 'vetghccbabvnpd', password: '98e7842dc3bc3c005360795492b171c1dc6c63924ac066fc897f5c32a94a990d'})
+      db = PG.connect({dbname: 'ddaa7cf7uq3tqn', host: 'ec2-23-21-216-174.compute-1.amazonaws.com', port: 5432, user: 'vetghccbabvnpd', password: '98e7842dc3bc3c005360795492b171c1dc6c63924ac066fc897f5c32a94a990d'})
       db.prepare("query", sql)
       result = db.exec_prepared("query", values)
     ensure
