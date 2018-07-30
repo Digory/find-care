d12aa05f9d7f91a0a4f13f46bb80282605e3c5c0
Digory <digory1@googlemail.com>
2018-07-30 19:09:57 +0100

    Fix syntax errors

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -15,7 +15,7 @@ class SqlRunner
 
   def self.run(sql, values = [])
     begin
-      db = PG.connect({dbname: 'dbeg0r2l0c1tq5', host: 'ec2-54-235-73-241.compute-1.amazonaws.com, port: 5432, user: 'qvosjfwhzvwosw', password: '0567686a4630e81bfc552437a5049c74ef4987e3161985039b602fb447de9b99'})
+      db = PG.connect({dbname: 'dbeg0r2l0c1tq5', host: 'ec2-54-235-73-241.compute-1.amazonaws.com', port: 5432, user: 'qvosjfwhzvwosw', password: '0567686a4630e81bfc552437a5049c74ef4987e3161985039b602fb447de9b99'})
       db.prepare("query", sql)
       result = db.exec_prepared("query", values)
     ensure
