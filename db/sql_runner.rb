db10e4696d201d0fd727450cb14cfd0e1c9bec8f
Digory <digory1@googlemail.com>
2018-08-04 19:11:36 +0100

    Link SQLRunner to Heroku database

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -2,20 +2,9 @@ require('pg')
 
 class SqlRunner
 
-  def self.run(sql, values = [])
-    begin
-      db = PG.connect({dbname: 'care', host: 'localhost'})
-      db.prepare("query", sql)
-      result = db.exec_prepared("query", values)
-    ensure
-      db.close() if db != nil
-    end
-    return result
-  end
-
   # def self.run(sql, values = [])
   #   begin
-  #     db = PG.connect({dbname: 'dbeg0r2l0c1tq5', host: 'ec2-54-235-73-241.compute-1.amazonaws.com', port: 5432, user: 'qvosjfwhzvwosw', password: '0567686a4630e81bfc552437a5049c74ef4987e3161985039b602fb447de9b99'})
+  #     db = PG.connect({dbname: 'care', host: 'localhost'})
   #     db.prepare("query", sql)
   #     result = db.exec_prepared("query", values)
   #   ensure
@@ -23,5 +12,16 @@ class SqlRunner
   #   end
   #   return result
   # end
+
+  def self.run(sql, values = [])
+    begin
+      db = PG.connect({dbname: 'dc8j1cmc3ln804', host: 'ec2-107-22-192-11.compute-1.amazonaws.com', port: 5432, user: 'qwridlezrefnzv', password: '8079860a1924a9742dcf8403545940c549c8846d2e44eac34bbb7f48ec2f424c'})
+      db.prepare("query", sql)
+      result = db.exec_prepared("query", values)
+    ensure
+      db.close() if db != nil
+    end
+    return result
+  end
 
 end
